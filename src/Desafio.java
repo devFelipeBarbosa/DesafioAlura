8ee4889342a66238a832167e3a129a99acc300a6
Felipe C. Barbosa <cavalcantebarbosa@gmail.com>
2024-09-04 06:57:53 -0300

    Inicializar dados do cliente

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Desafio {
+    public static void main(String[] args) {
+        String name = "Clark Kent";
+        String typeAccount = "Corrente";
+        double balance = 1599.99;
+
+        System.out.println("*************************************");
+        System.out.println("Nome do cliente: " + name);
+        System.out.println("Tipo conta: " + typeAccount);
+        System.out.println("Saldo atual: " + balance);
+        System.out.println("*************************************");
+       
+
+    }
+
+
+
 }
